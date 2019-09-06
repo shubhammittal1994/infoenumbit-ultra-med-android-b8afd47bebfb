a06985dcd17ef0bc0f038fee6395b9da256dd49d
unknown <you@example.com>
2019-09-06 23:18:05 +0530

    uc

--- app/src/main/java/com/soccermat/ultramed/activity/LoginActivity.java
+++ app/src/main/java/com/soccermat/ultramed/activity/LoginActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.soccermat.ultramed.R;
 import com.soccermat.ultramed.connection.RetrofitClient;
 
@@ -85,6 +86,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
     boolean validateInfo(){
+
+
+
 
         if (edittext_email.getText().toString().trim().length() > 0) {
             if (edittext_email.getText().toString().trim().matches(emailPattern)) {
